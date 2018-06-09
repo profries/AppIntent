48e254926f9ed692b53c6595ddd7b93a50d7b3d8
Luis Henrique Ries <prof.ries@gmail.com>
2018-06-09 14:41:03 -0300

    AppIntent - Exemplo de intent implicita

---
+++ app/src/main/java/com/example/iossenac/appintent/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.iossenac.appintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void abrirTelaEspecial(View view){
+        Intent it = new Intent("TELA_ESPECIAL");
+        startActivity(it);
+    }
+}
